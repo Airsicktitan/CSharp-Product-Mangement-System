1322200b9cc9c88abb1140e6d4f6614c03004300
airsicktitan <adamcwhooper@yahoo.com>
2024-12-16 00:36:50 -0500

    Updated Edit and Delete functions

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace ConsoleApp6
 
         static void Menu()
         {
+            var connectionString = GetConnectionString();
             var title = "Please make a selection:";
             string[] options =
                 [
@@ -48,10 +49,10 @@ namespace ConsoleApp6
                 switch (user_selection.ToString().ToUpper())
                 {
                     case "A":
-                        AddProduct(GetConnectionString());
+                        AddProduct(connectionString);
                         return;
                     case "B":
-                        ViewProducts(GetConnectionString());
+                        ViewProducts(connectionString);
                         return;
                     case "C":
                         invalidOptionEdit:
@@ -61,7 +62,31 @@ namespace ConsoleApp6
                         try
                         {
                             id_edit = int.Parse(user_selection_id_edit);
-                            EditProduct(GetConnectionString(), id_edit);
+
+                            using SqlConnection connection = new(connectionString);
+                            connection.Open();
+
+                            var checkItem = $"SELECT * FROM Product WHERE ID = @ID";
+
+                            using SqlCommand command = new(checkItem, connection);
+                            command.Parameters.AddWithValue("@ID", id_edit);
+
+                            using SqlDataReader reader = command.ExecuteReader();
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    int productID = reader.GetInt32(0);
+                                    string productName = reader.GetString(1);
+                                    EditProduct(connectionString, productID, productName);
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("ProductID not found. Try again.");
+                                goto invalidOptionEdit;
+                            }
                         }
                         catch
                         {
@@ -77,7 +102,30 @@ namespace ConsoleApp6
                         try
                         {
                             id_delete = int.Parse(user_selection_id_delete);
-                            DeleteProduct(GetConnectionString(), id_delete);
+
+                            using SqlConnection connection = new(connectionString);
+                            connection.Open();
+
+                            var checkItem = $"SELECT * FROM Product WHERE ID = @ID";
+
+                            using SqlCommand command = new(checkItem, connection);
+                            command.Parameters.AddWithValue("@ID", id_delete);
+
+                            using SqlDataReader reader = command.ExecuteReader();
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    int productID = reader.GetInt32(0);
+                                    string productName = reader.GetString(1);
+                                    DeleteProduct(connectionString, productID, productName);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("ProductID not found. Try again.");
+                                goto invalidOptionDelete;
+                            }
                         }
                         catch
                         {
@@ -91,15 +139,13 @@ namespace ConsoleApp6
                     default:
                         Console.WriteLine("Invalid selection.  Try again.");
                         break;
-
                 }
             }
-
         }
 
         static void AddProduct(string connectionString)
         {
-            var title = "Adding A New Product (press r to return)";
+            var title = "Adding A New Product (press r anytime to return)";
             Console.Clear();
             Console.WriteLine(title);
             for (int i = 0; i < title.Length; i++) Console.Write("_");
@@ -116,11 +162,29 @@ namespace ConsoleApp6
                 Console.Write("Name: ");
                 name = Console.ReadLine();
 
+                if (name.ToLower() == "r")
+                {
+                    Console.Clear();
+                    Menu();
+                }
+
                 Console.Write("Description: ");
                 description = Console.ReadLine();
 
+                if (description.ToLower() == "r")
+                {
+                    Console.Clear();
+                    Menu();
+                }
+
                 Console.Write("Quantity: ");
                 quantity = Console.ReadLine();
+
+                if (quantity.ToLower() == "r")
+                {
+                    Console.Clear();
+                    Menu();
+                }
 
                 try
                 {
@@ -138,7 +202,13 @@ namespace ConsoleApp6
                 Console.Write("Available (Yes or No): ");
                 isAvailable = Console.ReadLine();
 
-                if(isAvailable.ToUpper() != "YES" && isAvailable.ToUpper() != "NO")
+                if (isAvailable.ToLower() == "r")
+                {
+                    Console.Clear();
+                    Menu();
+                }
+
+                if (isAvailable.ToUpper() != "YES" && isAvailable.ToUpper() != "NO")
                 {
                     Console.WriteLine("Invalid selection.  Please Type Yes or No.");
                     isAvailable = string.Empty;
@@ -148,6 +218,12 @@ namespace ConsoleApp6
 
                 Console.Write("Sale (Yes or No): ");
                 isOnSale = Console.ReadLine();
+
+                if (isOnSale.ToLower() == "r")
+                {
+                    Console.Clear();
+                    Menu();
+                }
 
                 if (isOnSale.ToUpper() != "YES" && isOnSale.ToUpper() != "NO")
                 {
@@ -209,7 +285,7 @@ namespace ConsoleApp6
 
         static void ViewProducts(string connectionString)
         {
-            var title = "Viewing All Products (press r to return)";
+            var title = "Viewing All Products (press r anytime to return)";
             Console.Clear();
             Console.WriteLine(title);
             for (int i = 0; i < title.Length; i++) Console.Write("_");
@@ -280,24 +356,36 @@ namespace ConsoleApp6
             }
         }
 
-        static void EditProduct(string connectionString, int id)
+        static void EditProduct(string connectionString, int id, string name)
         {
-            var title = $"Editing {id} (press r to return)"; // To contain a product name later
+            var title = $"Editing {id} - {name} (press r anytime to return)"; // To contain a product name later
             Console.Clear();
             Console.WriteLine(title);
             for (int i = 0; i < title.Length; i++) Console.Write("_");
             Console.WriteLine();
 
+            string[] options =
+                [
+                    "A) Edit Product Name",
+                    "B) Edit Product Description",
+                    "C) Edit Product Quantity",
+                    "D) Edit Product Available",
+                    "E) Edit Product Sale"
+                ];
+            Console.WriteLine();
+
+            foreach(string option in options) Console.WriteLine(option);
+
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+
+            var columnToEdit = string.Empty;
+            var newValue = string.Empty;
+
             while (true)
             {
                 Console.Write("\nOption: ");
                 var user_selection = Console.ReadLine();
-
-                if (String.IsNullOrEmpty(user_selection))
-                {
-                    Console.WriteLine("Please select a value");
-                    continue;
-                }
 
                 switch (user_selection.ToString().ToUpper())
                 {
@@ -305,21 +393,131 @@ namespace ConsoleApp6
                         Console.Clear();
                         Menu();
                         return;
-                    default:
-                        Console.WriteLine("Invalid selection. Try again.");
+                    case "A":
+                        Console.Write("New Name: ");
+                        columnToEdit = "Name";
+                        newValue = Console.ReadLine();
+
                         break;
+                    case "B":
+                        Console.Write("New Description: ");
+                        columnToEdit = "Description";
+                        newValue = Console.ReadLine();
+
+                        break;
+                    case "C":
+                        Console.Write("New Quantity: ");
+                        columnToEdit = "Quantity";
+                        newValue = Console.ReadLine();
+
+                        break;
+                    case "D":
+                        Console.Write("New Availability (Yes or No): ");
+                        columnToEdit = "isAvailable";
+                        newValue = Console.ReadLine()?.ToUpper();
+                        if(newValue != "YES" &&  newValue != "NO")
+                        {
+                            Console.WriteLine("Invalid selection.  Please select Yes or No.");
+                            continue;
+                        }
+                        newValue = (newValue == "YES") ? "1" : "0";
+
+                        break;
+                    case "E":
+                        Console.Write("New Sale Status (Yes or No): ");
+                        columnToEdit = "isOnSale";
+                        newValue = Console.ReadLine();
+                        if (newValue != "YES" && newValue != "NO")
+                        {
+                            Console.WriteLine("Invalid selection.  Please select Yes or No.");
+                            continue;
+                        }
+                        newValue = (newValue == "YES") ? "1" : "0";
+
+                        break;
+                    default:
+                        Console.Write("Invalid selection. Try again.");
+                        continue;
+                }
+
+                if (!string.IsNullOrEmpty(columnToEdit) && !string.IsNullOrEmpty(newValue))
+                {
+                    var updateSQLQuery = $"UPDATE PRODUCT SET {columnToEdit} = @NewValue WHERE ID = @ID";
+                    using SqlCommand command = new(updateSQLQuery, connection);
+                    command.Parameters.AddWithValue("@NewValue", newValue);
+                    command.Parameters.AddWithValue("@ID", id);
+
+                    try
+                    {
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine($"{columnToEdit} updated successfully!");
+                        Console.WriteLine($"Row(s) Affected: {rowsAffected}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+                Console.WriteLine("Do you want to make another edit? (Yes or No)");
+                var again = Console.ReadLine()?.ToUpper();
+
+                if (again == "NO")
+                {
+                    Console.Clear();
+                    Menu();
+                    return;
+                }
+                else if (again != "YES")
+                {
+                    Console.WriteLine("Invalid selection, returning to main menu.");
+                    Console.Clear();
+                    Menu();
+                    return;
                 }
             }
         }
 
-        static void DeleteProduct(string connectionString, int id)
+        static void DeleteProduct(string connectionString, int id, string name)
         {
-            var title = $"Deleting {id} (press r to return)";  // To contain a product name later
+            var title = $"Deleting {id} - {name} (press r anytime to return)";  // To contain a product name later
             Console.Clear();
             Console.WriteLine(title);
             for (int i = 0; i < title.Length; i++) Console.Write("_");
             Console.WriteLine();
 
+
+            var deleteQuery = "DELETE FROM PRODUCT WHERE ID = @ID";
+
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+
+            using SqlTransaction transaction = connection.BeginTransaction();
+            using SqlCommand command = new(deleteQuery, connection, transaction);
+            command.Parameters.AddWithValue("@ID", id);
+
+
+            Console.Write("Are you sure you want to delete? (Yes or No): ");
+            var user_response = Console.ReadLine();
+            if(user_response.ToUpper() == "YES")
+            {
+                try
+                {
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                    Console.WriteLine("Product deleted successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    transaction.Rollback();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Deletion cancelled: ");
+                transaction.Rollback();
+            }
 
             while (true)
             {
